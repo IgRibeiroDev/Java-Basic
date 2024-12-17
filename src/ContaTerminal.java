a62eff9c05bf5a73795e52b79381e83f68791bb5
IgRibeiroDev <igribeiro.dev@gmail.com>
2024-12-16 21:07:15 -0300

    Inserindo dados pelo terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta:");
+        int numConta = scanner.nextInt();
+
+        System.out.println("Digite seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Digite seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia);
+        System.out.println("O número da sua conta é " + numConta);
+        System.out.println("Seu saldo de R$" + saldo + ", já está disponivel para saque.");
     }
     
 }
